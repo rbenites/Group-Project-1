7772838ff425c3b713bbd4fde86dc77d1db941d0
rbenites <rbenites115@gmail.com>
2018-04-04 08:33:53 -0700

    added Firebase

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -8,3 +8,16 @@ $.ajax({
 }).then(function (response) {
     console.log(response);
 });
+
+
+//Firebase
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD55v0OO7fbqD_SZqP0D4bw-2DrC5GUpDQ",
+    authDomain: "group-project-1-ca0dc.firebaseapp.com",
+    databaseURL: "https://group-project-1-ca0dc.firebaseio.com",
+    projectId: "group-project-1-ca0dc",
+    storageBucket: "group-project-1-ca0dc.appspot.com",
+    messagingSenderId: "906231816914"
+  };
+  firebase.initializeApp(config);
