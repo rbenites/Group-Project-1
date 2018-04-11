a6f4ac926046688b7035d36dfb4e70db0335373d
Brian Dawkins <bkdawkins@gmail.com>
2018-04-10 20:12:16 -0700

    app.js pseudocode and google maps api, weather api, and location api

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -33,19 +33,21 @@ $.ajax ({
   url: queryURLLocation,
   method: "GET"
 })
+// this gets the location from the IP address from wunderground
 .then(function(response){
   $("#location").html("You are in " + response.location.city);
   $("#long").html(response.location.lon + " Longitude");
   $("#lat").html(response.location.lat + " Latitude");
 
 
-
-
+// grabbing the city name and location from the api 
+// cityName grabs the name of the city from the API for use on the open weather map api 
 var cityName = response.location.city;
+
 // open weather map api
 var queryURLWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName
 + "&units=imperial&APPID=1faeb28f5befbdab79657bc5ee510ef3";
-
+// this query url gets the city name from the other api and generates information from it 
 $.ajax ({
    url: queryURLWeather,
    method: "GET"
@@ -56,6 +58,7 @@ $.ajax ({
 
 
 // google maps api
+// the google maps api also takes the city name from the wunderground api
 var mapsEmbed = $("<iframe>");
 mapsEmbed.attr("src", "https://www.google.com/maps/embed/v1/search?key=AIzaSyCF_5x7AkAOH8T7ijrquPSF5Lo3dullSiA&q=" + cityName)
 mapsEmbed.attr("width", "600");
