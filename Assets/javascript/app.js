d57bafa7f8d9b0d2d836fe5bda0722909691405f
Avi Davis Mac <oodavis.avi@gmail.com>
2018-04-10 20:33:16 -0700

    Start the form process

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,27 +1,81 @@
 /*jshint esversion: 6 */
 //SENDBIRD 
-var appId="C467D171-6024-43EC-9943-FE2E0478AFBD";
-var apiToken="623cd37c96fc7faf33dd41b5e68894169d567d91";
+var appId = "C467D171-6024-43EC-9943-FE2E0478AFBD";
+var apiToken = "623cd37c96fc7faf33dd41b5e68894169d567d91";
+var name = '';
+var age = '';
+var number = '';
+var person = '';
+var gender = '';
+var d = new Date();
 
 
+$("input-group").on("click", function () {
+  console.log($('input[name=person]:radio'));
+  console.log(gender);
+});
+
+//console.log(person.val());
 //Ajax 
 var queryURL = "https://api.sendbird.com";
 $.ajax({
-    url: queryURL,
-    method: "GET"
+  url: queryURL,
+  method: "GET"
 }).then(function (response) {
-    console.log(response);
+  console.log(response);
 });
 
 
 //Firebase
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyD55v0OO7fbqD_SZqP0D4bw-2DrC5GUpDQ",
-    authDomain: "group-project-1-ca0dc.firebaseapp.com",
-    databaseURL: "https://group-project-1-ca0dc.firebaseio.com",
-    projectId: "group-project-1-ca0dc",
-    storageBucket: "group-project-1-ca0dc.appspot.com",
-    messagingSenderId: "906231816914"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD55v0OO7fbqD_SZqP0D4bw-2DrC5GUpDQ",
+  authDomain: "group-project-1-ca0dc.firebaseapp.com",
+  databaseURL: "https://group-project-1-ca0dc.firebaseio.com",
+  projectId: "group-project-1-ca0dc",
+  storageBucket: "group-project-1-ca0dc.appspot.com",
+  messagingSenderId: "906231816914"
+};
+firebase.initializeApp(config);
+database = firebase.database();
+$(document).ready(function () {
+  jsSetup();
+  $("input[name='gender']").on("click", function() {
+    alert($(this).val());
+});
+});
+
+function processForm() {
+  console.log($('#rezQForm'));
+  $('#rezQForm').submit(function (e) {
+
+    e.preventDefault();
+    var date = d.toLocaleString([], {
+      hour12: true
+    });
+    name = $("#inputName").val().trim();
+    // age = parseInt($("#inputAge").val().trim());
+    // number = parseInt($("#inputNumber").val().trim());
+    person = $('input[name=person]:checked').val();
+    gender = $('input[name=gender]:checked').val();
+
+
+    database.ref().push({
+      date: date,
+      name: name,
+      // age: age,
+      // number: number,
+      person: person,
+      gender: gender
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
+  });
+}
+
+function jsSetup() {
+  processForm();
+
+
+}
